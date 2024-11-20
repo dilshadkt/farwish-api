0b1ab257a25cd86d7038112c4af85339d396c44c
Dilshad <hmydilshadkt@gmail.com>
2024-11-20 18:56:30 +0530

    feat : fix

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -47,7 +47,8 @@ router.post("/reset-password", resetPassword);
 // some dashboad thinks like the total counts and some thing 😒😒
 router.get("/dashboard-stats", verifyUser, getDashboardStat);
 router.get("/withdrawal-requests", verifyUser, getWithdrawal);
-router.get("/withdrawal-requests/:id", verifyUser, getSingleWithdrawal);
+// router.get("/withdrawal-requests/:id", verifyUser, getSingleWithdrawal);
+router.get("/withdrawal-requests/:id", getSingleWithdrawal);
 router.put("/withdrawal-requests/:id", verifyUser, updateWithdrawal);
 
 module.exports = router;
